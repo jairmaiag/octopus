856a05610b1f266cad21c03e7f9dd7941d6c6bb9
Itamar Nunes <itamarbel@gmail.com>
2020-07-31 20:07:04 -0300

    fix: test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,14 +1,14 @@
 export default {
-  "database": {
+  'database': {
     database: process.env.DATABASENAME || 'carp',
     username: process.env.DATABASEUSERNAME || 'carp',
     password: process.env.DATABASEPASSWORD || 'carp',
     host: process.env.HOST || '127.0.0.1',
-    dialect: "postgres",
+    dialect: 'postgres',
     freezeTableName: true,
     timestamps: true
   },
-  "server": {
+  'server': {
     port: process.env.PORT || 80
   }
 }
